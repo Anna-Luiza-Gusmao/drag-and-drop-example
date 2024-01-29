ccea59c50386237ddb74ed1ecc044baf15dd05d7
Anna Luiza Gusmão <87679298+Anna-Luiza-Gusmao@users.noreply.github.com>
2024-01-29 10:36:00 -0300

    style: added responsiveness to main screens

--- src/components/Example/styles.ts
+++ src/components/Example/styles.ts
@@ -27,6 +27,13 @@ export const DragAndDropContainer = styled.section`
         background: ${props => props.theme.colors["blue-600"]};
         border-radius: 8px;
     }
+
+    @media (min-width: 320px) and (max-width: 1024px) {
+        justify-content: space-between;
+    }
+    @media (min-width: 2560px) {
+        gap: 3.5rem;
+    }
 `
 
 export const ColumnBox = styled.div`
@@ -45,6 +52,24 @@ export const ColumnBox = styled.div`
         -webkit-box-shadow: 3px 9px 42px -6px ${props => props.theme.colors["black-700"]};
         -moz-box-shadow: 3px 9px 42px -6px ${props => props.theme.colors["black-700"]};
         box-shadow: 3px 9px 42px -6px ${props => props.theme.colors["black-700"]};
+    }
+
+    @media (min-width: 320px) and (max-width: 480px) {
+        min-width: 20rem;
+    }
+    @media (min-width: 481px) and (max-width: 1024px) {
+        min-width: 24rem;
+    }
+    @media (min-width: 1025px) and (max-width: 1280px) {
+        min-width: 22rem;
+    }
+    @media (min-width: 2560px) {
+        min-width: 36rem;
+        font-size: 1.5rem;
+
+        h3 {
+            font-size: 1.75rem;
+        }
     }
 `
 
--- src/styles.ts
+++ src/styles.ts
@@ -23,4 +23,64 @@ export const Container = styled.div`
     h2 {
         font-weight: 400;
     }
+
+    @media (min-width: 320px) and (max-width: 480px) {
+        gap: 1.5rem;
+        padding: 2rem;
+
+        header {
+            width: 90%;
+        }
+
+        h1 {
+            padding-bottom: 0.75rem;
+        }
+    
+        h2 {
+            font-size: 1.25rem;
+        }
+    }
+    @media (min-width: 481px) and (max-width: 768px) {
+        gap: 1.5rem;
+        padding: 3rem;
+
+        header {
+            width: 70%;
+        }
+
+        h1 {
+            padding-bottom: 0.75rem;
+        }
+    
+        h2 {
+            font-size: 1.25rem;
+        }
+    }
+    @media (min-width: 769px) and (max-width: 1280px) {
+        gap: 2rem;
+        padding: 3rem;
+    
+        header {
+            width: 80%;
+        }
+    
+        h1 {
+            padding-bottom: 0.75rem;
+        }
+    }
+    @media (min-width: 2560px) {
+        padding: 6rem;
+    
+        header {
+            width: 70%;
+        }
+    
+        h1 {
+            font-size: 3rem;
+        }
+    
+        h2 {
+            font-size: 2.25rem;
+        }
+    }
 `
